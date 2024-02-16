6721d12759c1c7c69ac7ecd583159c8bb1412996
Mariana Mozzer Arantes <mozzerm@yahoo.com.br>
2024-02-16 11:08:32 -0300

    att

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import BootstrapVueNext from 'bootstrap-vue-next'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import BootstrapVueNext from 'bootstrap-vue-next';
+import axios from 'axios';
+import store from './store';
 
-// import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css';
 
-import store from './store'
 
-const app = createApp(App).use(store)
-app.use(router)
-app.use(BootstrapVueNext)
-app.mount('#app')
+const app = createApp(App).use(store);
+app.use(router);
+app.use(BootstrapVueNext);
+
+
+const isApp1 = 'http://192.168.0.5:8000/api';
+const backendUrl = isApp1 ? 'http://192.168.0.5:8080/api' : 'http://192.168.0.6:8000/api';
+axios.defaults.baseURL = backendUrl;
+
+
+app.mount('#app');
